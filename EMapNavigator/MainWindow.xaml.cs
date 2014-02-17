10e6a94c8340bf23b692117450e4d980c3b34cc2
Evgeniy Plyusnin <eninbox@gmail.com>
2014-02-17 15:50:12 +0600

    Visual-квадратики забегали по поверхности

--- EMapNavigator/MainWindow.xaml.cs
+++ EMapNavigator/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace EMapNavigator
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            map.Move(10, 2);
+        }
     }
 }
