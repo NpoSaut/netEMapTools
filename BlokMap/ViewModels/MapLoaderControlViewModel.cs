81576da3ed1c921c84ab0a8cc3190ff85522fdf8
Evgeniy Plyusnin <eninbox@gmail.com>
2016-12-16 18:21:00 +0500

    После открытия .gps-карты, карта перескакиывает на первый попавшийся столб

--- BlokMap/ViewModels/MapLoaderControlViewModel.cs
+++ BlokMap/ViewModels/MapLoaderControlViewModel.cs
@@ -1,7 +1,9 @@
-﻿using System.Reactive;
+﻿using System.Linq;
+using System.Reactive;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using GMapElements;
+using MapViewer.Mapping;
 using Microsoft.Win32;
 using ReactiveUI;
 
@@ -12,11 +14,13 @@ namespace BlokMap.ViewModels
         private readonly ReactiveCommand<Unit> _load;
         private readonly IMapLoadingService _mapLoadingService;
         private readonly MapPresenter _mapPresenter;
+        private readonly IMappingService _mappingService;
 
-        public MapLoaderControlViewModel(IMapLoadingService MapLoadingService, MapPresenter MapPresenter)
+        public MapLoaderControlViewModel(IMapLoadingService MapLoadingService, MapPresenter MapPresenter, IMappingService MappingService)
         {
             _mapLoadingService = MapLoadingService;
             _mapPresenter = MapPresenter;
+            _mappingService = MappingService;
             _load = ReactiveCommand.CreateAsyncTask((_, c) => LoadMap());
         }
 
@@ -35,6 +39,7 @@ namespace BlokMap.ViewModels
 
             _mapPresenter.PrintPosts(gMap);
             _mapPresenter.PrintObjects(gMap, 1);
+            _mappingService.Navigate(gMap.Sections.First().Posts.First().Point);
         }
     }
 }
