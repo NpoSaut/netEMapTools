654ceec8d44673b2c2b63534fba9ba1e7408bd0a
Evgeniy Plyusnin <eninbox@gmail.com>
2017-01-12 11:18:39 +0500

    Теперь для километровых столбов показываются ещё и содержащиеся в нём пути

--- BlokMap/MapElements/KilometerPostMapElement.cs
+++ BlokMap/MapElements/KilometerPostMapElement.cs
@@ -11,7 +11,8 @@ namespace BlokMap.MapElements
 {
     public class KilometerPostMapElement : MapPointElement
     {
-        public GPost Post { get; set; }
+        protected static readonly SolidColorBrush TextBackgroundBrush = new SolidColorBrush(Colors.White);
+        private readonly Brush mainBrush = Brushes.DarkSlateGray;
 
         public KilometerPostMapElement(GPost Post) : base(Post.Point)
         {
@@ -19,21 +20,22 @@ namespace BlokMap.MapElements
             SectionBrush = Brushes.Aquamarine;
         }
 
-        private Brush mainBrush = Brushes.DarkSlateGray;
+        public GPost Post { get; set; }
+
         public Brush SectionBrush { get; set; }
 
         protected override void DrawPointElement(DrawingContext dc)
         {
-            var postLabelText = string.Format("{0}км", (double)Post.Ordinate / 1000);
+            string postLabelText = string.Format("{0}км", (double)Post.Ordinate / 1000);
             var postLabel = new FormattedText(postLabelText, CultureInfo.CurrentCulture,
-                                         FlowDirection.LeftToRight, new Typeface("Verdana"), 10, mainBrush);
+                                              FlowDirection.LeftToRight, new Typeface("Verdana"), 10, mainBrush);
 
             const int flagHeight = 22;
 
             dc.PushTransform(new TranslateTransform(0, -flagHeight));
 
-            dc.DrawRectangle(Brushes.White, new Pen(mainBrush, 1), new Rect(-0.5, -0.5, Math.Round((double)postLabel.Width) + 5, Math.Round((double)postLabel.Height) + 2));
-            dc.DrawText(postLabel, new Point(2,0));
+            dc.DrawRectangle(Brushes.White, new Pen(mainBrush, 1), new Rect(-0.5, -0.5, Math.Round(postLabel.Width) + 5, Math.Round(postLabel.Height) + 2));
+            dc.DrawText(postLabel, new Point(2, 0));
             dc.DrawLine(new Pen(mainBrush, 2), new Point(0, 0), new Point(0, flagHeight));
 
             dc.Pop();
@@ -42,28 +44,28 @@ namespace BlokMap.MapElements
 
             if (IsMouseOver)
             {
-                /*dc.PushTransform(new TranslateTransform(3, 10));
-                foreach (var track in Post.Tracks)
-                {
-                    var h = PrintTrack(dc, track);
-                    dc.PushTransform(new TranslateTransform(0, h + 1));
-                }
-                foreach (var track in Post.Tracks) dc.Pop();
-                dc.Pop();*/
-
                 dc.PushTransform(new TranslateTransform(3, 10));
                 PrintStack(dc,
-                    new FormattedText(Post.Direction == OrdinateDirection.Increasing ? "Возрастает по неч." : "Убывает по чёт.", CultureInfo.CurrentCulture, FlowDirection.LeftToRight, new Typeface("Verdana"), 10, Brushes.DarkBlue));
+                           new FormattedText(Post.Direction == OrdinateDirection.Increasing ? "Возрастает по неч." : "Убывает по чёт.",
+                                             CultureInfo.CurrentCulture, FlowDirection.LeftToRight, new Typeface("Verdana"), 10, Brushes.DarkBlue),
+                           new FormattedText(String.Format("Пути: {0}", string.Join(", ", Post.Tracks.Select(TrackName))),
+                                             CultureInfo.CurrentCulture, FlowDirection.LeftToRight, new Typeface("Verdana"), 10, Brushes.DarkOliveGreen));
                 dc.Pop();
             }
         }
 
-        protected static readonly SolidColorBrush TextBackgroundBrush = new SolidColorBrush(Colors.White);
+        private string TrackName(GTrack Track)
+        {
+            if (Track.Number <= 15)
+                return string.Format("{0}П", Track.Number);
+            return string.Format("{0}Н", Track.Number - 15);
+        }
+
         protected static void PrintStack(DrawingContext dc, params FormattedText[] labels)
         {
             dc.DrawRectangle(TextBackgroundBrush, null, new Rect(-2, -1, labels.Max(l => l.Width) + 2, labels.Sum(l => l.Height + 1) + 2));
             double yOffset = 0;
-            foreach (var label in labels)
+            foreach (FormattedText label in labels)
             {
                 dc.DrawText(label, new Point(0, yOffset));
                 yOffset += label.Height + 1;
@@ -72,7 +74,8 @@ namespace BlokMap.MapElements
 
         protected double PrintTrack(DrawingContext dc, GTrack Track)
         {
-            var postLabel = new FormattedText(string.Format((string)"Путь {0}", (object)Track.Number), CultureInfo.CurrentCulture, FlowDirection.LeftToRight, new Typeface("Verdana"), 10, mainBrush);
+            var postLabel = new FormattedText(string.Format("Путь {0}", Track.Number), CultureInfo.CurrentCulture, FlowDirection.LeftToRight,
+                                              new Typeface("Verdana"), 10, mainBrush);
             dc.DrawText(postLabel, new Point());
             return postLabel.Height;
         }
@@ -92,6 +95,5 @@ namespace BlokMap.MapElements
         }
 
         #endregion
-
     }
 }
