bef27da619628dbcb150868014a4f8f780c36ea1
Evgeniy Plyusnin <eninbox@gmail.com>
2017-01-20 16:48:48 +0500

    Русификация названий объектов

--- BlokMap/MapElements/MapObjectElements/MapUnknownObjectElement.cs
+++ BlokMap/MapElements/MapObjectElements/MapUnknownObjectElement.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Media;
 using Geographics;
@@ -9,6 +10,22 @@ namespace BlokMap.MapElements.MapObjectElements
     public class MapUnknownObjectElement : MapObjectElement
     {
         private const Double Size = 8;
+
+        private static readonly Dictionary<GObjectType, string> _typeNames =
+            new Dictionary<GObjectType, string>
+            {
+                { GObjectType.Bridge, "Мост" },
+                { GObjectType.Crossing, "Переезд" },
+                { GObjectType.DangerousPlace, "Огр." },
+                { GObjectType.DeadEnd, "Тупик" },
+                { GObjectType.GpuSaut, "Генератор" },
+                { GObjectType.Platform, "Платформа" },
+                { GObjectType.Station, "Станция" },
+                { GObjectType.Switch, "Стрелка" },
+                { GObjectType.Tks, "ТКС" },
+                { GObjectType.TrafficLight, "Светофор" },
+                { GObjectType.Tunnel, "Тоннель" }
+            };
 
         public MapUnknownObjectElement(EarthPoint Position, GObject Target) : base(Position, Target) { }
 
@@ -23,7 +40,7 @@ namespace BlokMap.MapElements.MapObjectElements
             dc.DrawLine(accentPen, new Point(0.5 * Size, -0.5 * Size), new Point(-0.5 * Size, 0.5 * Size));
 
             if (Zoom > 12)
-                PrintDetails(dc, String.Format("{0} \"{1}\"", Target.Type, Target.Name));
+                PrintDetails(dc, String.Format("{0} \"{1}\"", _typeNames[Target.Type], Target.Name));
         }
     }
 }
