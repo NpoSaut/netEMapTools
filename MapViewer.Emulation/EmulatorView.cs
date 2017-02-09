debe235c7d14e3c3252d02de0ec0a5db11b6eb5e
Evgeniy Plyusnin <eninbox@gmail.com>
2017-02-09 19:23:34 +0500

    Сделал так, чтобы чуть красивее отображались эмуляторы в правом столбце

---
+++ MapViewer.Emulation/EmulatorView.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace MapViewer.Emulation
+{
+    public static class EmulatorView
+    {
+        public static readonly DependencyProperty EmulatorNameProperty = DependencyProperty.RegisterAttached(
+            "EmulatorName", typeof (string), typeof (EmulatorView), new PropertyMetadata(default(string)));
+
+        public static void SetEmulatorName(DependencyObject element, string value) { element.SetValue(EmulatorNameProperty, value); }
+
+        public static string GetEmulatorName(DependencyObject element) { return (string)element.GetValue(EmulatorNameProperty); }
+    }
+}
