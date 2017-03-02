0e2ab070b5d0b3029920752bb56273115e279c43
Evgeniy Plyusnin <eninbox@gmail.com>
2017-03-02 11:50:47 +0500

    Прячу BlokMapOverlay, если карта не загружена

--- BlokMap/ViewModels/BlokMapOverlayViewModel.cs
+++ BlokMap/ViewModels/BlokMapOverlayViewModel.cs
@@ -1,11 +1,26 @@
-﻿namespace BlokMap.ViewModels
+﻿using System.Reactive.Linq;
+using ReactiveUI;
+
+namespace BlokMap.ViewModels
 {
-    public class BlokMapOverlayViewModel
+    public class BlokMapOverlayViewModel : ReactiveObject
     {
-        public BlokMapOverlayViewModel(TrackSelectorViewModel TrackSelector, BlokMapSearchViewModel Search)
+        private readonly ObservableAsPropertyHelper<bool> _isActive;
+
+        public BlokMapOverlayViewModel(TrackSelectorViewModel TrackSelector, BlokMapSearchViewModel Search, IBlokMapService BlokMapService)
         {
             this.TrackSelector = TrackSelector;
             this.Search = Search;
+
+            Observable.FromEventPattern(h => BlokMapService.CurrentMapChanged += h,
+                                        h => BlokMapService.CurrentMapChanged -= h)
+                      .Select(_ => BlokMapService.CurrentMap != null)
+                      .ToProperty(this, x => x.IsActive, out _isActive, false);
+        }
+
+        public bool IsActive
+        {
+            get { return _isActive.Value; }
         }
 
         public TrackSelectorViewModel TrackSelector { get; private set; }
