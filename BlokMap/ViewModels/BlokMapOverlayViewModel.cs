96149ca2bdc18929c7e6202e4e146bd4165397f9
Evgeniy Plyusnin <eninbox@gmail.com>
2017-03-20 14:32:10 +0500

    Отображение в BlokMapOverlayView имени файла, даты и номера карты

--- BlokMap/ViewModels/BlokMapOverlayViewModel.cs
+++ BlokMap/ViewModels/BlokMapOverlayViewModel.cs
@@ -1,11 +1,16 @@
-﻿using System.Reactive.Linq;
+﻿using System;
+using System.IO;
+using System.Reactive.Linq;
 using ReactiveUI;
 
 namespace BlokMap.ViewModels
 {
     public class BlokMapOverlayViewModel : ReactiveObject
     {
+        private readonly ObservableAsPropertyHelper<string> _fileName;
         private readonly ObservableAsPropertyHelper<bool> _isActive;
+        private readonly ObservableAsPropertyHelper<string> _mapConversionDate;
+        private readonly ObservableAsPropertyHelper<ushort> _mapNumber;
 
         public BlokMapOverlayViewModel(TrackSelectorViewModel TrackSelector, BlokMapSearchViewModel Search, IBlokMapService BlokMapService)
         {
@@ -20,6 +25,30 @@ namespace BlokMap.ViewModels
 
             mapChanged.Select(map => map.map != null)
                       .ToProperty(this, x => x.IsActive, out _isActive);
+
+            mapChanged.Select(map => Path.GetFileNameWithoutExtension(map.file))
+                      .ToProperty(this, x => x.MapFileName, out _fileName);
+
+            mapChanged.Select(map => map.map.Header.Number)
+                      .ToProperty(this, x => x.MapNumber, out _mapNumber);
+
+            mapChanged.Select(map => map.map.Header.ConversionDate.ToShortDateString())
+                      .ToProperty(this, x => x.MapConversionDate, out _mapConversionDate);
+        }
+
+        public string MapConversionDate
+        {
+            get { return _mapConversionDate.Value; }
+        }
+
+        public ushort MapNumber
+        {
+            get { return _mapNumber.Value; }
+        }
+
+        public string MapFileName
+        {
+            get { return _fileName.Value; }
         }
 
         public bool IsActive
