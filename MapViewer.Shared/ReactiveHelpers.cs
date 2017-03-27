7105ef1d47630a8f457b731330aa32a541f65187
Evgeniy Plyusnin <eninbox@gmail.com>
2017-03-27 16:09:02 +0500

    Добавлен класс ReactiveHelpers

---
+++ MapViewer.Shared/ReactiveHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace MapViewer
+{
+    public static class ReactiveHelpers
+    {
+        public static IObservable<T> DisposePrevious<T>(this IObservable<T> Source)
+            where T : IDisposable
+        {
+            return Source.Scan((previous, current) =>
+                               {
+                                   previous.Dispose();
+                                   return current;
+                               });
+        }
+    }
+}
