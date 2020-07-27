53f38d27b767f38e455082e56a39ba28788d92ab
Evgeniy Plyusnin <eninbox@gmail.com>
2020-07-27 13:16:22 +0500

    Переведено на новый формат InstallerTools без использования Nuke (самописный вариант)

---
+++ build/Parameters.cs
@@ -0,0 +1,10 @@
+﻿using CommandLine;
+
+public class Parameters
+{
+    [Option('o', "publishplace", Required = false, Default = null, HelpText = "Place to publish the installer.")]
+    public string PublishPlace { get; set; }
+
+    [Option('c', "configuration", Required = false, Default = "Release", HelpText = "Configuration to publish.")]
+    public string Configuration { get; set; }
+}
